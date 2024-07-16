83acb5d3f09cc81b1f6f62369f29a7bbd9751c20
Jakub Bartušek <jack.bartusek@gmail.com>
2024-07-16 21:43:47 +0200

    init setup

---
+++ Assets/Hra/Scripts/Globals/GlobalConstants.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalConstants
+{
+    public enum Tags
+    {
+        Player,
+        BreakableCube,
+        TutorialCamera,
+        TutorialCollider
+    }
+}
